3b1976cf8cf3939e7102d449156c00905405909d
cristian <127469110+NarvaezSKY@users.noreply.github.com>
2023-08-30 00:57:46 -0500

    so

--- proyectoFiltro/js/pruebas.js
+++ proyectoFiltro/js/pruebas.js
@@ -3,24 +3,26 @@ import { rutas } from "/proyectoFiltro/modules/db.js";
 
 
 document.addEventListener('DOMContentLoaded', ()=>{
-
-    // VARIABLES
     let nombreRuta=document.getElementById('nombre')
     let minKm=document.getElementById('minkm')
     let maxKm=document.getElementById('maxkm')
     let estadoRuta=document.getElementById('estado')
     let calificacionRuta=document.getElementById('calificacion')
     let tipoVia=document.getElementById('tipovia')
+    // VARIABLES
+    let arrayOpciones=[]
+    arrayOpciones.push(nombreRuta,estadoRuta,calificacionRuta, tipoVia)
+  
 
     let resultadoBusqueda=document.getElementById('resultado')
-    mostrarRutas()
+    // mostrarRutas()
     
     rutas.forEach(element => {
         // console.log(element)
 
     // poblar opciones
         let opcNombre=document.createElement('option')
-        opcNombre.value=element.nombre
+        opcNombre.value=element
         opcNombre.textContent=element.nombre
         nombreRuta.appendChild(opcNombre)
 
@@ -36,77 +38,71 @@ document.addEventListener('DOMContentLoaded', ()=>{
     });
     
 // FUNCION QUE MUESTRA LAS RUTAS
-    function mostrarRutas(){
-        rutas.forEach((element)=>{
-            let divElemento=document.createElement('div')
-            divElemento.classList.add('divElemento')
-            divElemento.innerHTML=`<b class="textoRuta">Nombre Ruta:</b> ${element.nombre}
-            <b class="textoRuta">Kilometros Min: </b> ${element.minKilometros}
-            <b class="textoRuta">Kilometros Max: </b> ${element.maxKilometros}
-            <b class="textoRuta">Estado: </b> ${element.estado}
-            <b class="textoRuta">Calificación: </b> ${element.calificacion}
-            <b class="textoRuta">Tipo de vía: </b> ${element.tipoVia}`
+    // function mostrarRutas(){
+    //     rutas.forEach((element)=>{
+    //         let divElemento=document.createElement('div')
+    //         divElemento.classList.add('divElemento')
+    //         divElemento.innerHTML=`<b class="textoRuta">Nombre Ruta:</b> ${element.nombre}
+    //         <b class="textoRuta">Kilometros Min: </b> ${element.minKilometros}
+    //         <b class="textoRuta">Kilometros Max: </b> ${element.maxKilometros}
+    //         <b class="textoRuta">Estado: </b> ${element.estado}
+    //         <b class="textoRuta">Calificación: </b> ${element.calificacion}
+    //         <b class="textoRuta">Tipo de vía: </b> ${element.tipoVia}`
 
-            resultadoBusqueda.appendChild(divElemento)
+    //         resultadoBusqueda.appendChild(divElemento)
 
        
-        })
-
-        
-    }
-
-    let filtro={}
-
-    nombreRuta.addEventListener('change', (e)=>{
-        filtro.nombre=e.target.value
-        console.log(filtro)
-    })
-    estadoRuta.addEventListener('change', (e)=>{
-        filtro.estado=e.target.value
-        console.log(filtro)
-    })
-    calificacionRuta.addEventListener('change', (e)=>{
-        filtro.calificacion=parseInt(e.target.value)
-        console.log(filtro)
-    })
-    tipoVia.addEventListener('change', (e)=>{
-        filtro.tipo=e.target.value
-        console.log(filtro)
-    })
-
-    for (const valor in filtro){
-        const value=filtro[valor]
-        console.log(value)
-    }
-    
-
-
-
-
-    // nombreRuta.addEventListener('change',(element)=>{
-    //     limpiarHTML()
-    //     console.clear()
-    //     console.log(element.target)
-    //     rutas.forEach((so)=>{
-    //         if (element.target.value===so.nombre){
-                
-    //             console.log(so)
-    //             let divElemento=document.createElement('div')
-    //             divElemento.classList.add('divElemento')
-    //             divElemento.innerHTML=`<b class="textoRuta">Nombre Ruta:</b> ${so.nombre}
-    //             <b class="textoRuta">Kilometros Min: </b> ${so.minKilometros}
-    //             <b class="textoRuta">Kilometros Max: </b> ${so.maxKilometros}
-    //             <b class="textoRuta">Estado: </b> ${so.estado}
-    //             <b class="textoRuta">Calificación: </b> ${so.calificacion}
-    //             <b class="textoRuta">Tipo de vía: </b> ${so.tipoVia}`
-    //             resultadoBusqueda.appendChild(divElemento)
-             
-    //         }
-            
     //     })
+
         
-        
-    // })
+    // }
+
+    function limpiarHTML(){
+        resultadoBusqueda.innerHTML=""
+    }
+
+arrayOpciones.forEach((e)=>{
+e.addEventListener('change', (element)=>{
+    limpiarHTML()
+    // console.clear()
+
+        for(const clave of rutas){
+            
+            for(const clavedo in clave){
+                
+                const valor=clave[clavedo]
+                
+
+                if (element.target.value===valor){
+                    console.log("a", valor)
+
+
+                    limpiarHTML()
+                    // rutas.forEach((so)=>{
+                    //     // console.log(element.target.value)
+                    //     let divElemento=document.createElement('div')
+                    //     divElemento.classList.add('divElemento')
+                    //     divElemento.innerHTML=`<b class="textoRuta">Nombre Ruta:</b> ${so.nombre}
+                    //     <b class="textoRuta">Kilometros Min: </b> ${so.minKilometros}
+                    //     <b class="textoRuta">Kilometros Max: </b> ${so.maxKilometros}
+                    //     <b class="textoRuta">Estado: </b> ${so.estado}
+                    //     <b class="textoRuta">Calificación: </b> ${so.calificacion}
+                    //     <b class="textoRuta">Tipo de vía: </b> ${so.tipoVia}`
+                    //     resultadoBusqueda.appendChild(divElemento)
+                    // })
+
+                    resultadoBusqueda.innerHTML=`${valor}`
+                 
+                 
+                }
+
+
+                
+            }
+        } 
+})
+})
+
     // estadoRuta.addEventListener('change',(element)=>{
     //     limpiarHTML()
     //     console.clear()
@@ -127,9 +123,11 @@ document.addEventListener('DOMContentLoaded', ()=>{
              
     //         }
             
-    //     })  
+    //     })
+        
+        
     // })
-
+    
     // tipoVia.addEventListener('change',(element)=>{
     //     limpiarHTML()
     //     console.clear()
@@ -159,9 +157,28 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 
 
-    function limpiarHTML(){
-        resultadoBusqueda.innerHTML=""
-    }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 })
